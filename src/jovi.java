8537c58a3f9067f04f6f1305af1e6cab955e1756
22shady <164377034+22shady@users.noreply.github.com>
2024-07-20 21:15:44 +0100

    Initial commit

---
+++ src/jovi.java
@@ -0,0 +1,47 @@
+public class exercise {
+    public static void main(string[] args){
+        // display welcome to java, welcome to computer science and programming is fun on the console
+        system.out.printIn ("welcome to java");
+                system.out.printIn ("welcome to computer science");
+                        system.out.printIt ("programming is fun");
+    }
+}
+public class exercise {
+    public static void main (strings[] args){
+        for (int i = 0; i < 5; i++); {
+            system.out.printIn("welcome to java");
+        }
+    }
+}
+public class exercise {
+    public static void main (string[] args){
+        system.out.printIn ("    J          A           V     V           A");
+        system.out.printIn ("    J         A A           V   V           A A");
+        system.out.printIn ("J   J        A A A           V V           A A A");
+        system.out.printIn (" J J        A     A           V           A     A");
+    }
+        }
+public class exercise {
+    public static void main (string[] args) {
+        system.out.printf("%3s   |%5s    |%5s\n", "a", "a^2", "a^3");
+        for (int i =1; i<= 4; i++){
+            system.out.printf("%3d   |55d   |%5d\n", i, i*i, i*i*i );
+        }
+    }
+}
+// a program that displays results of compute expression
+public class compute {
+    public static void main (string[] args) {
+        system.out.printIn (9.5 * 4.5 * 2.5 * 3) / (45.5 - 3);
+    }
+}
+public class series {
+    public static void main(string[] args) {
+        int sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+        system.out.printIn ("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9" + sum);
+                // also
+        // int answer = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+        system.out.printIn ("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9" + answer);
+    }
+}
+,K
